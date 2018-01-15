cd2fd8784bd8c16f727efdf295510fddc5b2d561
qyl1006 <yuelinshi@qq.com>
2018-01-15 23:54:12 +0800

    1.15

---
+++ workspace/IO/src/com/_520it/day01/_01_file/FileDemo.java
@@ -0,0 +1,33 @@
+package com._520it.day01._01_file;
+
+import java.io.File;
+
+
+//系统平台的分隔符
+public class FileDemo {
+	public static void main(String[] args) {
+		String str = File.pathSeparator;
+		System.out.println(str);
+		char c = File.pathSeparatorChar;
+		System.out.println(c);
+		
+		
+		String str2 = File.separator;
+		char c2 = File.separatorChar;
+		System.out.println(str2);
+		System.out.println(c2);
+		System.out.println("===============");
+		
+		//创建File对象
+		File f = new File("C/abc/123.test");
+		System.out.println(f);
+		
+		File f2 = new File("C/abc/123", "anb.txt");
+		System.out.println(f2);
+		
+		File parent = new File("C/abc/123");
+		File f3 = new File(parent, "123.txt");
+		System.out.println(f3);
+		
+	}
+}
