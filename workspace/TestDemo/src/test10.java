92d238d9db6c4ac633c69d735dfa8858bb9d0810
qyl1006 <yuelinshi@qq.com>
2018-01-17 15:01:25 +0800

    1.16Demo

--- workspace/TestDemo/src/test10.java
+++ workspace/TestDemo/src/test10.java
@@ -4,22 +4,11 @@ import java.util.LinkedList;
 
 public class test10 {
 	public static void main(String[] args) {
-		BB b = new BB();
-		System.out.println(b.get());
-		BB bb = b;
-		b.test();
-		System.out.println(b.get());
+		String str = "dafj,osf";
+		String[] strs = str.split(",");
+		for(String ele : strs){
+			String s = ele.substring(0,1).toUpperCase() + ele.substring(1, ele.length());
+			System.out.println(s);
+		}
 	}
-}
-
-class BB{
-	private Integer i = 321;
-	
-	public Integer get(){
-		return i;
-	}
-	public void test(){
-		i ++;
-	}
-	
 }
